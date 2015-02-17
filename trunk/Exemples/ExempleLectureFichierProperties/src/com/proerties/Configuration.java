c169884a2c9b4beb1959a581a628f48a0320402e
sekhi.massinissa@gmail.com <sekhi.massinissa@gmail.com@68794633-6a48-cfe0-4876-02e1cd1b3c7f>
2015-02-17 10:42:09 +0000

    tutorial montrant le concept de connexion via api jdbc

---
+++ trunk/Exemples/ExempleLectureFichierProperties/src/com/proerties/Configuration.java
@@ -0,0 +1,52 @@
+package com.proerties;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Configuration {
+
+	private static final String fileName = "resources/constants.properties"; //$NON-NLS-1$	
+
+	private static Configuration _instance = null;
+
+	private Properties props = null;
+
+	private Configuration() {
+	         props = new Properties();
+	    	try {
+		    FileInputStream fis = new FileInputStream(new File(fileName));
+		    props.load(fis);
+	    	}
+	    	catch (Exception e) {
+	    	    // catch Configuration Exception right here
+	    	}
+	    }
+
+	public synchronized static Configuration getInstance() {
+		if (_instance == null)
+			_instance = new Configuration();
+		return _instance;
+	}
+
+	// get property value by name
+	public String getProperty(String key) {
+		String value = null;
+		if (props.containsKey(key))
+			value = (String) props.get(key);
+		else {
+			// the property is absent
+		}
+		return value;
+	}
+	
+	public static void main(String[] args) throws IOException {
+
+		String fileNameDomaine = Configuration.getInstance().getProperty("entete.compo");
+		System.out.println(fileNameDomaine);
+
+	}
+	
+
+}
