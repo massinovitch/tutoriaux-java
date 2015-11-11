d39521955f32aa9e891267c258af916963d7fa9e
massinovitch <sekhi.massinissa@gmail.com>
2015-11-11 23:33:51 +0100

    jdom exemple

--- src/com/properties/LevelSearch.java
+++
@@ -1,7 +0,0 @@
-package com.properties;
-
-public final class LevelSearch {
-	public static final String PHRASE = "PHRASE";
-	public static final String PARAGRAPHE = "PARAGRAPHE";
-	public static final String DOCUMENT = "DOCUMENT";
-}
